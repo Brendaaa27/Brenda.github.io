f35f1598c9e40216be2d16c4a3ee6934eaa9fa7f
BREN <lyjing27@gmail.com>
2017-09-30 23:52:40 +1000

    add js

---
+++ myProfile.js
@@ -0,0 +1,15 @@
+
+window.onload = function(){
+    console.log('Hello World');
+
+    var myName = "Yangjing Liu"
+    console.log(myName)
+
+    function disp_alert()
+    {
+        alert("Hello this is Brenda!");
+    }
+
+
+}
+
