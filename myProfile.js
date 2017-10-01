5f1aa7a10230596f2e2b02628e7c72f4c85a1db7
BREN <lyjing27@gmail.com>
2017-10-02 00:17:06 +1000

    edit js of profile

--- myProfile.js
+++ myProfile.js
@@ -5,11 +5,31 @@ window.onload = function(){
     var myName = "Yangjing Liu"
     console.log(myName)
 
-    function disp_alert()
+    function myFunction()
     {
         alert("Hello this is Brenda!");
     }
 
+    document.getElementById("myName").onclick = function() {myFunction()};
+    function myFunction() {
+        document.getElementById("myName").innerHTML = "刘阳婧";
+    }
+
+    $(Document).ready(function(){
+        $("h2").click(function(){
+            $(".h2p").toggle(1000);
+        });
+    });
+
+    $(document).ready(function() {
+        $(".profileimg-wrapper").mouseover(function () {
+            $(".profileimg-wrapper p").
+            });
+    });
+
+    }
+
+
+
 
 }
-
